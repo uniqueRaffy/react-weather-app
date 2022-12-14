0ca20fa70657d70bfc8eb44503a1e116f24273dc
Rafiat Salewa <idrisrafiat91@gmail.com>
2022-12-14 15:28:20 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This Project was coded by Rafiat Salewa and it is an{" "}
+        <a
+          href="https://github.com/uniqueRaffy/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-source on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
