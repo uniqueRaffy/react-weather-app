7613255461fb51e6c1c75578d04549fe08b37e87
Rafiat Salewa <idrisrafiat91@gmail.com>
2022-12-14 17:26:12 +0100

    Added Boostrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This Project was coded by Rafiat Salewa and it is an{" "}
-        <a
-          href="https://github.com/uniqueRaffy/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-source on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This Project was coded by Rafiat Salewa and it is an{" "}
+          <a
+            href="https://github.com/uniqueRaffy/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      {" "}
+      <h1>Lagos</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+    </div>
+  );
+}
