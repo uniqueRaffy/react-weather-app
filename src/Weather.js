2db950a51773fae9478cd3d3f0004e48e2048179
Rafiat Salewa <idrisrafiat91@gmail.com>
2022-12-15 14:52:37 +0100

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,11 +5,52 @@ export default function Weather() {
   return (
     <div className="Weather">
       {" "}
+      <form>
+        <div className="row ">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city....."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-success w-100"
+            />
+          </div>
+        </div>
+      </form>
       <h1>Lagos</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="mostly cloudy"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">29</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 10%</li>
+            <li>Humidity: 81%</li>
+            <li>Wind: 11km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
